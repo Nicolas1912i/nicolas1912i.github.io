ca208111641d8a5692bb3468411c121345eb441f
Nicolas Arias Escudero <anonymmajora@gmail.com>
2023-04-01 20:35:05 -0500

    I think this one is final (Really)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "b9fd85d295e7f0be5b4cc1cc6a5d5a57",
+  "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"main.dart.js": "b9fd85d295e7f0be5b4cc1cc6a5d5a57",
 "version.json": "fb8d4678888024cd5f83d8522bb3a722",
 "assets/assets/page-1/images/sunset-gif.png": "b556abe5db5f29727309b7ffcb405e9a",
 "assets/assets/page-1/images/sunset-gif-izq.png": "b556abe5db5f29727309b7ffcb405e9a",
@@ -20,8 +25,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8f8bc1baaffddfa102686fa6c5aa379c",
-"/": "8f8bc1baaffddfa102686fa6c5aa379c",
+"index.html": "96669dd71f235b765e53e0aba8a0f3d0",
+"/": "96669dd71f235b765e53e0aba8a0f3d0",
 "manifest.json": "4137147246a11ba2148318bc696c4202"
 };
 
